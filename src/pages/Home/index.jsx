935e9e816a1bbe6351de0dbbd78828c7ceab7dce
GustBitencourt <gubiten@gmail.com>
2021-11-09 15:41:13 -0300

    material icon, input, englobando menu e criando espaço do map

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field/dist/index';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
-const Home = () => <Container>Hello World</Container>;
+import { Container, Search, Logo, Wrapper } from './styles';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do restaurante" />
+          <TextField
+            label="Pesquisar Restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.primary};
+/* Responsável por englobar menu lateral e o mapa */
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+/* Menu lateral */
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background}
+  width: 22.5rem;
+  heigth: 100vh;
+  overflow-y: auto;
+`;
+
+/* input de texto pra search */
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 1rem;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 1rem;
+`;
+
+export const Map = styled.div`
+  background-color: blue;
+  width: 31.25rem;
 `;
